9950d65e3d8f78ecc60c817c4ce5d5266ab13fd4
yamanerkam <erkamyaman35@gmail.com>
2024-12-29 17:37:45 +0300

    data-binding

---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<div class="page">
+    <h1>Svelte 101</h1>
+
+    <span class="val">
+        {number}
+    </span>
+
+    <div class="buttons">
+        <button on:click={addPlusOne} >+</button>
+        <button on:click={subsctractMinusOne}>-</button>
+    </div>
+
+    <div class="custom-update buttons">
+        <h3>Custom Update</h3>
+        <input bind:value={updateValue} />
+        <label>
+        <input
+                type="radio"
+                name="operator"
+                value="+"
+                bind:group={selectedOperator}
+        />
+        +
+        </label>
+        <label>
+            <input
+                    type="radio"
+                    name="operator"
+                    value="-"
+                    bind:group={selectedOperator}
+            />
+            -
+        </label>
+        <button on:click={()=>customOperation(selectedOperator,updateValue)}>Update</button>
+    </div>
+
+</div>
+
+
+
+<style>
+    page{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    .val{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    }
+
+    .buttons{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+    }
+</style>
+
+<script lang="ts"   >
+    let number : Number = 0;
+    let updateValue : Number = 0;
+    let selectedOperator : String = '+';
+
+    function addPlusOne(){
+        number++;
+    }
+
+    function subsctractMinusOne(){
+        number--;
+    }
+
+    function customOperation(operation:string, value:number){
+        if(operation === '+'){
+            number = Number(number) + Number(value);
+        }else if(operation ==='-'){
+            number = number - value;
+        }
+        updateValue = 0;
+    }
+</script>
